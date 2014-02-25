dd31b3a88e4ffe3d0353bc50c89f0445233fd6c2
Ryan Shoff <ryanshoff@gmail.com>
2014-02-24 22:21:00 -0600

    test auto

--- TMW2013/Commands/ShootSlowCommand.cpp
+++ TMW2013/Commands/ShootSlowCommand.cpp
@@ -51,9 +51,11 @@ bool ShootSlowCommand::IsFinished() {
 // Called once after isFinished returns true
 void ShootSlowCommand::End() {
 	Robot::picker->StopShooter();
+   	printf("ShootSlowCommand end \r\n");
 }
 // Called when another command which requires one or more of the same
 // subsystems is scheduled to run
 void ShootSlowCommand::Interrupted() {
 	Robot::picker->StopShooter();
+   	printf("ShootSlowCommand interrupted\r\n");
 }
--- TMW2013/Commands/threeballauto.cpp
+++ TMW2013/Commands/threeballauto.cpp
@@ -5,9 +5,17 @@
 #include "DeployRear.h"
 
 ThreeBallAuto::ThreeBallAuto() {
+	DriverStation * DS = DriverStation::GetInstance();
+
 	AddSequential(new DeployRear(1.)); // throw out back wait 1 second
 	AddSequential(new ShootSlowCommand(NULL)); // bring down arm
-	AddSequential(new Drive(.3, 0, 0, 1.0)); // drive forward
+	if( DS->GetDigitalIn(1)) {
+		AddSequential(new Drive(.4, .3, 0, 1.0)); // drive forward
+		AddSequential(new Drive(0, -.4, 0, 1.3)); // drive forward
+	}
+	if( DS->GetDigitalIn(2)) {
+		AddSequential(new Drive(.75, 0, 0, 1.0)); // drive forward
+	}
 	AddSequential(new WaitCommand(1.0)); // wait to let ball settle
 	AddSequential(new ShootCommand(NULL)); // shoot
 //	AddSequential(new Unfold());
